7a013cd7b68dd86300108a79c054b3a5617382ed
Raehan <raehan900809@gmail.com>
2022-01-27 16:39:36 +0900

    add: tailwind css

---
+++ coding-apple/1st/shop/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import DefaultLayout from './layouts/DefaultLayout'
+import NavBars from './layouts/NavBars'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBars></NavBars>
+    </div>
+  );
+}
+
+export default App;
