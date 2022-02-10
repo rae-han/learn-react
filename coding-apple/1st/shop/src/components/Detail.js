48a7f1b04e873d8de3a815661da31a29ac56cf9b
Raehan <raehan900809@gmail.com>
2022-02-10 20:12:17 +0900

    add: react transition group as node package

--- coding-apple/1st/shop/src/components/Detail.js
+++ coding-apple/1st/shop/src/components/Detail.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
-import { useHistory, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+
+import './Detail.scss';
 
 const Detail = ({ list }) => {
   let history = useHistory();
   let { id } = useParams();
+  let [category, setCategory] = useState(1);
+  let [toggle, setToggle] = useState(false);
 
   const item = list.find(i => i.id === parseInt(id, 10));
+
+  const toggleCategory = id => {
+    setToggle(false);
+    setCategory(id);
+  }
 
   return (
     <div>
@@ -19,9 +29,35 @@ const Detail = ({ list }) => {
           <p>{item.price}</p>
         </div>
       </div>
+      <div>
+        <div>
+          <button onClick={() => toggleCategory(1)}>first</button>
+          <button onClick={() => toggleCategory(2)}>second</button>
+          <button onClick={() => toggleCategory(3)}>third</button>
+        </div>
+        <div>
+          <CSSTransition in={toggle} classNames="wow" timeout={500}>
+            <TabContent category={category} setToggle={setToggle}></TabContent>
+          </CSSTransition>
+        </div>
+      </div>
     </div>
   );
 };
+
+function TabContent({ category, setToggle }) {
+  useEffect(() => {
+    setToggle(true)
+  });
+
+  return(
+    <div style={{ backgroundColor: 'green' }}>
+      { category === 1 && 1111 }
+      { category === 2 && 2222 }
+      { category === 3 && 3333 }
+    </div>
+  )
+}
 
 export default Detail;
 
