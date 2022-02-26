3c7a799a28a70b997b64e6d6984951ce1a96c0ef
raehan <raehan900809@gmail.com>
2022-02-26 23:59:47 +0900

    add: code

--- skill-in-dealing-with-react-3rd/6/sample/src/App.js
+++ skill-in-dealing-with-react-3rd/6/sample/src/App.js
@@ -1,23 +1,23 @@
+import react, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState([1, 2, 3])
+
+  const addItem = () => setNumber(number.concat(number[number.length-1]+1));
+  // const deleteItem = id => setNumber(number.filter(item => item!==id));
+  const deleteItem = id => {
+    console.log(id)
+    setNumber(number.filter(item => item!==id))
+  };
+  
+  const list = number.map(item => <li onClick={() => deleteItem(item)} key={item}>{item}</li>)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addItem}>+</button>
+      <ul>{list}</ul>
     </div>
   );
 }
