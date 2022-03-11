8fe4f28f10918cabaf8710b71f6bf9d69733f606
Raehan <raehan900809@gmail.com>
2022-03-11 21:21:57 +0900

    add: api router

--- skill-in-dealing-with-react-3rd/blog/blog-backend/src/index.js
+++ skill-in-dealing-with-react-3rd/blog/blog-backend/src/index.js
@@ -4,6 +4,8 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
+const api = require('./api')
+
 router.get('/', ctx => {
   ctx.body = '홈';
 });
@@ -11,6 +13,8 @@ router.get('/', ctx => {
 router.get('/about', ctx => {
   ctx.body = '소개';
 });
+
+router.use('/api', api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
 
