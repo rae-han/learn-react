e84d48598e24fc246fa7512f935e5c4b231a3a63
raehan <raehan900809@gmail.com>
2022-03-13 00:41:59 +0900

    add: api auth

--- skill-in-dealing-with-react-3rd/blog/blog-backend/src/api/index.js
+++ skill-in-dealing-with-react-3rd/blog/blog-backend/src/api/index.js
@@ -1,11 +1,13 @@
 // const Router = require('koa-router');
 // const posts = require('./posts');
 import Router from "koa-router";
-import posts from './posts'
+import posts from './posts';
+import auth from './auth';
 
 const api = new Router();
 
 api.use('/posts', posts.routes());
+api.use('/auth', auth.routes());
 
 // module.exports = api;
 export default api;
