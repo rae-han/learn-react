cfb9c6cbd26adc54bacceaa86af514d3a1f28848
raehan <raehan900809@gmail.com>
2022-03-14 14:19:59 +0900

    add custom action creator func add: create redux saga

--- skill-in-dealing-with-react-3rd/blog/blog-frontend/src/modules/auth.js
+++ skill-in-dealing-with-react-3rd/blog/blog-frontend/src/modules/auth.js
@@ -1,8 +1,13 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import { createRequestActionTypes } from '../lib/createRequestSaga';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes('auth/REGISTER');
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes('auth/LOGIN');
+
 
 export const changeField = createAction(CHANGE_FIELD, ({ form, key, value }) => ({ form, key, value }));
 export const initializeForm = createAction(INITIALIZE_FORM, form => form);
