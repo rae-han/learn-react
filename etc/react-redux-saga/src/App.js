3f3ab8c4c9c668fd9c121be3980f174ce89f0a53
Raehan <raehan900809@gmail.com>
2022-04-15 18:00:52 +0900

    add: react redux saga base

---
+++ etc/react-redux-saga/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import SampleReactReduxSaga from './components/SampleReactReduxSaga';
+
+function App() {
+  return (
+    <div className="App">
+      <SampleReactReduxSaga></SampleReactReduxSaga>
+    </div>
+  );
+}
+
+export default App;
---
+++ etc/react-redux-saga/src/components/SampleReactReduxSaga.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SampleReactReduxSaga = () => {
+  return (
+    <div>
+      <h1>ToDo List</h1>
+      <div>
+        <input type="text" />
+        <button>++</button>
+      </div>
+      <ul>
+        <li><span>asdf</span><button>--</button></li>
+      </ul>
+    </div>
+  );
+};
+
+export default SampleReactReduxSaga;
