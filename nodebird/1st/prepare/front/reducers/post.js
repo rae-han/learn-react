b7e43566ca4ea2c5d4a2f3b0850f54c8621de284
Raehan <raehan900809@gmail.com>
2022-05-30 19:12:35 +0900

    refactor: use immer for immutabillty

--- nodebird/1st/prepare/front/reducers/post.js
+++ nodebird/1st/prepare/front/reducers/post.js
@@ -1,4 +1,5 @@
 import shortid from "shortid";
+import produce from "immer";
 
 export const initialState = {
   mainPosts: [
@@ -101,76 +102,54 @@ export const addComment = (data) => ({
   data,
 })
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => produce(state, (draft) => {
   switch (action.type) {
     case ADD_POST_REQUEST:
-      return {
-        ...state,
-        addPostLoading: true,
-        addPostDone: false,
-        addPostError: null,
-      }
+      draft.addPostLoading = true;
+      draft.addPostDone= false;
+      draft.addPostError= null;
+      break;
     case ADD_POST_SUCCESS:
-      return {
-        ...state,
-        mainPosts: [dummyPost(action.data), ...state.mainPosts],
-        addPostLoading: false,
-        addPostDone: true,
-      }
+      draft.mainPosts.unshift(dummyPost(action.data));
+      draft.addPostLoading = false;
+      draft.addPostDone = true;
+      break;
     case ADD_POST_FAILURE:
-      return {
-        ...state,
-        addPostLoading: false,
-        addPostError: action.error,
-      }
+      draft.draddPostLoading = false;
+      draft.draddPostError = action.error;
+      break;
     case REMOVE_POST_REQUEST:
-      return {
-        ...state,
-        removePostLoading: true,
-        removePostDone: false,
-        removePostError: null,
-      }
+      draft.removePostLoading = true;
+      draft.removePostDone = false;
+      draft.removePostError = null;
+      break;
     case REMOVE_POST_SUCCESS:
-      return {
-        ...state,
-        mainPosts: state.mainPosts.filter(post => post.id !== action.data),
-        removePostLoading: false,
-        removePostDone: true,
-      }
+      draft.mainPosts = draft.mainPosts.filter(post => post.id !== action.data);
+      draft.removePostLoading = false;
+      draft.removePostDone = true;
+      break;
     case REMOVE_POST_FAILURE:
-      return {
-        ...state,
-        removePostLoading: false,
-        removePostError: action.error,
-      }
+      draft.removePostLoading = false;
+      draft.removePostError = action.error;
+      break;
     case ADD_COMMENT_REQUEST:
-      return {
-        ...state,
-        addCommentLoading: true,
-        addCommentDone: false,
-        addCommentError: null,
-      }
+      draft.addCommentLoading = true;
+      draft.addCommentDone = false;
+      draft.addCommentError = null;
+      break;
     case ADD_COMMENT_SUCCESS:
-      console.log(action.data)
-      const postIndex = state.mainPosts.findIndex(post => post.id === action.data.postId);
-      console.log(postIndex)
-      return {
-        ...state,
-        mainPosts: state.mainPosts.map(post => post.id === action.data.postId ? (
-          { ...post, Comments: [ dummyComment(action.data), ...post.Comments ] }
-        )  : post),
-        addCommentLoading: false,
-        addCommentDone: true,
-      }
+      const post = draft.mainPosts.find(post => post.id === action.data.postId);
+      post.Comments.unshift(dummyComment(action.data.content));
+      draft.addCommentLoading = false;
+      draft.addCommentDone = true;
+      break;
     case ADD_COMMENT_FAILURE:
-      return {
-        ...state,
-        addCommentLoading: false,
-        addCommentError: action.error,
-      }
+      draft.addCommentLoading = false;
+      draft.addCommentError = action.error;
+      break;
     default:
-      return state;
+      break;
   }
-};
+});
 
 export default reducer;
--- nodebird/1st/prepare/front/reducers/user.js
+++ nodebird/1st/prepare/front/reducers/user.js
@@ -1,4 +1,5 @@
 import shortId from "shortid";
+import produce from 'immer';
 
 export const initialState = {
   // isLoggedIn: false, // 로그인 돼 있는지
@@ -136,108 +137,72 @@ const dummyUser = data => ({
   ],
 })
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => produce(state, (draft) => {
   switch (action.type) {
     case LOG_IN_REQUEST: // 3-2. 이때 조심해야할 것이 user 객체에 있는 내용을 initialState에 바로 넣었기 때문에 뎁스가 1단계 줄었다.
-    console.log('3. reducer/login log in requrest')
-      return {
-        ...state,
-        logInLoading: true,
-        logInError: null,
-        logInDone: false,
-      }
+      draft.logInLoading = true;
+      draft.logInError = null;
+      draft.logInDone = false;
+      break;
     case LOG_IN_SUCCESS: 
-      console.log('5. reducer login success')
-      return {
-        ...state,
-        logInLoading: false,
-        logInDone: true,
-        me: dummyUser(action.data)
-      }
+      draft.logInLoading = false;
+      draft.logInDone = true;
+      draft.me = dummyUser(action.data);
+      break;
     case LOG_IN_FAILURE:
-      return {
-        ...state,
-        logInLoading: false,
-        logInError: action.error,
-      }
+      draft.logInLoading = false;
+      draft.logInError = action.error;
+      break;
     case LOG_OUT_REQUEST:
-      return {
-        ...state,
-        logOutLoading: true,
-        logOutDone: false,
-        logOutError: null,
-      }
+      draft.logOutLoading = true;
+      draft.logOutDone = false;
+      draft.logOutError = null;
+      break;
     case LOG_OUT_SUCCESS:
-      return {
-        ...state,
-        logOutLoading: false,
-        logOutDone: true,
-        me: null
-      }
+      draft.logOutLoading = false;
+      draft.logOutDone = true;
+      draft.me = null;
+      break;
     case LOG_OUT_FAILURE:
-      return {
-        ...state,
-        logOutLoading: false,
-        logOutError: action.error
-      }
+      draft.logOutLoading = false;
+      draft.logOutError = action.error;
+      break;
     case SIGN_UP_REQUEST:
-      return {
-        ...state,
-        signUpLoading: true,
-        signUpDone: false,
-        signUpError: null,
-      }
+      draft.signUpLoading = true;
+      draft.signUpDone = false;
+      draft.signUpError = null;
+      break;
     case SIGN_UP_SUCCESS:
-      return {
-        ...state,
-        signUpLoading: false,
-        signUpDone: true,
-      }
+      draft.signUpLoading = false;
+      draft.signUpDone = true;
+      break;
     case SIGN_UP_FAILURE:
-      return {
-        ...state,
-        signUpLoading: false,
-        signUpError: action.error
-      }
+      draft.signUpLoading = false;
+      draft.signUpError = action.error;
+      break;
     case CHANGE_NICKNAME_REQUEST:
-      return {
-        ...state,
-        changeNicknameLoading: true,
-        changeNicknameDone: false,
-        changeNicknameError: null,
-      }
+      draft.changeNicknameLoading = true;
+      draft.changeNicknameDone = false;
+      draft.changeNicknameError = null;
+      break;
     case CHANGE_NICKNAME_SUCCESS:
-      return {
-        ...state,
-        changeNicknameLoading: false,
-        changeNicknameDone: true,
-      }
+      draft.changeNicknameLoading = false;
+      draft.changeNicknameDone = true;
+      break;
     case CHANGE_NICKNAME_FAILURE:
-      return {
-        ...state,
-        changeNicknameLoading: false,
-        changeNicknameError: action.error
-      }
+      draft.changeNicknameLoading = false;
+      draft.changeNicknameError = action.error;
+      break;
     case ADD_POST_TO_ME:
-      console.log(action)
-      return {
-        ...state,
-        me: {
-          ...state.me,
-          Posts: [{ id: action.data }, ...state.me.Posts]
-        }
-      }
+      draft.me.Posts.unshift({ id: action.data });
+      break;
     case REMOVE_POST_OF_ME:
-      return {
-        ...state,
-        me: {
-          ...state.me,
-          Posts: state.me.Posts
-        }
-      }
+      draft.me.Posts = draft.me.Posts.filter((post) => post.id !== action.data);
+      break;
     default:
-      return state;
+      break;
   }
-};
+});
+
 
 export default reducer;
