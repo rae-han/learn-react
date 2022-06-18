be89f8af7171fd459979440c88d4cb5cd5070429
raehan <raehan900809@gmail.com>
2022-06-19 01:40:31 +0900

    add: retweet

--- nodebird/1st/prepare/back/routes/posts.js
+++ nodebird/1st/prepare/back/routes/posts.js
@@ -33,6 +33,15 @@ router.get('/', async (req, res, next) => {
         model: User,
         as: 'Likers',
         attributes: ['id'],
+      }, {
+        model: Post,
+        as: 'Retweet',
+        include: [{
+          model:User,
+          attributes: ['id', 'nickname'],
+        }, {
+          model: Image,
+        }]
       }]
     });
 
