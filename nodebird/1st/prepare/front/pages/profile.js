6010bd1dcca839f7523c8b1e8de3a8637eb78aa0
raehan <raehan900809@gmail.com>
2022-07-31 11:16:03 +0900

    add: profile page with ssr

--- nodebird/1st/prepare/front/pages/profile.js
+++ nodebird/1st/prepare/front/pages/profile.js
@@ -5,10 +5,12 @@ import Router from 'next/router';
 import useSWR from 'swr';
 import axios from "axios";
 
+import wrapper from '../store/configureStore';
+import { LOAD_FOLLOWERS_REQUEST, LOAD_FOLLOWINGS_REQUEST} from '../reducers/user'
+
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
-import { LOAD_FOLLOWERS_REQUEST, LOAD_FOLLOWINGS_REQUEST} from '../reducers/user'
 
 const fetcher = (url) => axios.get(url, { withCredentials: true}).then((result) => result.data);
 
@@ -72,5 +74,21 @@ function Profile() {
     </>
   );
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  console.log('getServerSideProps start');
+  console.log(context.req.headers);
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  if (context.req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+  context.store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+  context.store.dispatch(END);
+  console.log('getServerSideProps end');
+  await context.store.sagaTask.toPromise();
+});
 
 export default Profile;
